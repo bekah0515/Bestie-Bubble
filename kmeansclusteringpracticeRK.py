3939e4f685dfa582a7ca07788bedb2cfa9d9f4f0
pfb2024 <pfb2024@info17.cshl.edu>
2024-10-26 19:13:12 -0400

    added RK scripts

--- kmeansclusteringpracticeRK.py
+++ kmeansclusteringpracticeRK.py
@@ -27,14 +27,26 @@ label = kmeans.fit_predict(df)
 print(label)
 
 ##VISUALIZE DATA
-#filter rows of original data for cluster 0
-filtered_label0 = df[label == 0]
-filtered_label2 = df[label == 2]
-filtered_label8 = df[label == 8]
+
+#filter rows of original data for cluster 0,2,8
+#filtered_label0 = df[label == 0]
+#filtered_label2 = df[label == 2]
+#filtered_label8 = df[label == 8]
 
 #plotting the results
-plt.scatter(filtered_label0[:,0] , filtered_label0[:,1])
-plt.scatter(filtered_label2[:,0] , filtered_label2[:,1] , color = 'red')
-plt.scatter(filtered_label8[:,0] , filtered_label8[:,1] , color = 'black')
+#plt.scatter(filtered_label0[:,0] , filtered_label0[:,1])
+#plt.scatter(filtered_label2[:,0] , filtered_label2[:,1] , color = 'red')
+#plt.scatter(filtered_label8[:,0] , filtered_label8[:,1] , color = 'black')
 
+#plt.show()
+
+#Getting unique labels
+u_labels = np.unique(label)
+ 
+#plotting the results:
+for i in u_labels:
+    plt.scatter(df[label == i , 0] , df[label == i , 1] , label = i)
+#show centroids
+plt.scatter(centroids[:,0] , centroids[:,1] , s = 80, color = 'k)
+plt.legend()
 plt.show()
