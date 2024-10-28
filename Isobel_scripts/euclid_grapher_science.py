34d267396f6541163fb2c98a3dff60b18cc0dce1
pfb2024 <pfb2024@info25.cshl.edu>
2024-10-27 20:39:28 -0400

    fixing overlapping text in plotly

--- Isobel_scripts/euclid_grapher_science.py
+++ Isobel_scripts/euclid_grapher_science.py
@@ -3,6 +3,10 @@
 import pandas, os
 import plotly.express as px
 
+# def improve_text_position(x):
+#     positions = ['top center', 'bottom center']
+#     return [positions[i % len(positions)] for i in range(len(x))]
+
 def similarity_grapher():
 
     similarity_dict = {}
@@ -45,6 +49,10 @@ def similarity_grapher():
     names[3] = sorted_sims[3] + " 🥉"
     names[len(sorted_dict) -1] = sorted_sims[len(sorted_dict)-1] + " 🐸"
 
+    text_position = ['top center'] * len(sorted_dict)
+    text_position[1] = 'bottom center'
+    text_position[3] = 'bottom center'
+
     column_label = ['Jaccard','Gower']
     similarities = pandas.DataFrame.from_dict(sorted_dict, orient='index', columns=column_label)
     similarities['color'] = colors
@@ -52,19 +60,33 @@ def similarity_grapher():
     similarities['y-axis'] = 0
     similarities['size'] = sizes
     similarities['name'] = names
+    similarities['textposition'] = text_position
 
     if not os.path.exists('images'):
         os.mkdir('images')
 
-    fig1 = px.scatter(similarities, x='Gower', y='y-axis', color='color', color_discrete_map='identity', symbol='marker', symbol_map='identity', size='size', text='name')
-    fig1.update_traces(offsetgroup=0, textposition='top center')
+
+    fig1 = px.scatter(similarities, x='Gower', y='y-axis', color='color', color_discrete_map='identity', symbol='marker', symbol_map='identity', size='size')
+    fig1.update_traces(offsetgroup=0)
+    fig1.update_yaxes(visible=False)
     fig1.update_layout(xaxis_title='Gower Similarity')
+    for distance in sorted_sims:
+        if names[sorted_sims.index(distance)] != "":
+            fig1.add_annotation(x=sorted_dict[distance]['Gower'],y=0, 
+                            text=names[sorted_sims.index(distance)], 
+                            showarrow=True, arrowhead=1, textangle=75)
     fig1.write_image(f"images/{sorted_sims[0]}_gower_matches.png", scale=2)
 
 
-    fig2 = px.scatter(similarities, x='Jaccard', y='y-axis', color='color', color_discrete_map='identity', symbol='marker', symbol_map='identity', size='size', text='name')
-    fig2.update_traces(offsetgroup=0, textposition='top center')
+    fig2 = px.scatter(similarities, x='Jaccard', y='y-axis', color='color', color_discrete_map='identity', symbol='marker', symbol_map='identity', size='size')
+    fig2.update_traces(offsetgroup=0)
+    fig2.update_yaxes(visible=False)
     fig2.update_layout(xaxis_title='Jaccard Similarity')
+    for distance in sorted_sims:
+        if names[sorted_sims.index(distance)] != "":
+            fig2.add_annotation(x=sorted_dict[distance]['Jaccard'],y=0, 
+                            text=names[sorted_sims.index(distance)], 
+                            showarrow=True, arrowhead=1, textangle=75)
     fig2.write_image(f"images/{sorted_sims[0]}_jaccard_matches.png", scale=2)
 
     print(f'Matches saved to images directory')
