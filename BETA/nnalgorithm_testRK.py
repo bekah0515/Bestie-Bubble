a776b9583d9b1ce66ec91f3116362139a4d8e1c3
pfb2024 <pfb2024@info17.cshl.edu>
2024-10-28 12:03:47 -0400

    updated script

--- BETA/nnalgorithm_testRK.py
+++ BETA/nnalgorithm_testRK.py
@@ -1,8 +1,10 @@
 #!/Users/pfb2024/mamba/envs/bio/bin/python
 import sys
-from nn_algorithmRK import nn_algorithm
+from nn_algorithmRK_fixed import nn_algorithm
 
 repfile = sys.argv[1]
 inputfile = sys.argv[2]
-top3matches = nn_algorithm(repfile, inputfile)
-print(top3matches)
+user_name, result_dict, list_of_matches = nn_algorithm(repfile, inputfile)
+print("UserName:", user_name)
+print("list of matches:", list_of_matches)
+print("result_dict:", result_dict)
